5a4d0378634f788582fbe85b120191ba18fe8268
micheal.wang <wangmk13@163.com>
2020-11-02 16:15:26 +0800

    feat: admin init

---
+++ src/main/java/com/micheal/itoken/admin/AdminApplication.java
@@ -0,0 +1,21 @@
+package com.micheal.itoken.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author <a href="mailto:wangmk13@163.com">micheal.wang</a>
+ * @date 2020/11/2 15:57
+ * @Description 分布式服务监控
+ */
+@EnableAdminServer
+@EnableEurekaClient
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
